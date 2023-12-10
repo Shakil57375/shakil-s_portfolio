e7d0094c1f9cfc9c56134e8ec24ff87c70213c9a
Shakil57375 <shakil57375@gmail.com>
2023-12-10 10:10:13 +0600

    floating navbar updated

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,20 +1,14 @@
 /* eslint-disable react/prop-types */
-import "./Button.css"
 const Button = ({ children }) => {
   return (
-    <svg viewBox="45 60 400 320" xmlns="http://www.w3.org/2000/svg">
-      <path
-        fill="#fff"
-        d="M 90 210 C 90 180 90 150 90 150 C 150 150 180 150 180 150 C 180 150 300 150 300 150 C 300 150 330 150 390 150 C 390 150 390 180 390 210 C 390 240 390 270 390 270 C 330 270 300 270 300 270 C 300 270 180 270 180 270 C 180 270 150 270 90 270 C 90 270 90 240 90 210"
-        mask="url(#knockout-text)"
-      ></path>
-      <mask id="knockout-text">
-        <rect width="100%" height="100%" fill="#fff" x="0" y="0" />
-        <text x="147" y="227" fill="#000">
-          {children}
-        </text>
-      </mask>
-    </svg>
+    <a
+      href="#_"
+      className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500"
+    >
+      <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
+      <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+      <span className="relative text-white">{children}</span>
+    </a>
   );
 };
 

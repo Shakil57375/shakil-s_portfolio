dd2f33943a95a78fac04bd6a4051e80b6f81cf39
Shakil57375 <shakil57375@gmail.com>
2023-12-19 22:12:41 +0600

    projects links added

--- src/Data/projectData.js
+++ src/Data/projectData.js
@@ -5,9 +5,11 @@ export const projects = [
     description:
       "This is a recipe website featuring top-rated chefs and their recipes from Bangladesh. To access the best chefs' recipes, you'll need to log in. On a chef's details page, you'll find in-depth information about the chef and their recipes.",
     src: "https://i.ibb.co/QF246Bh/Recipe-hunter.png",
-    technologies : "react, express.js, tailwind, css",
+    technologies: "react, express.js, tailwind, css",
     link: "https://www.ignant.com/2023/03/25/ad2186-matthias-leidingers-photographic-exploration-of-awe-and-wonder/",
     color: "rgb(15 118 110)",
+    github: "https://github.com/Shakil57375/chef-hunter-client",
+    liveLink: "https://assignment-ten-1b552.web.app/",
   },
   {
     title: "Toy Generator",
@@ -15,18 +17,22 @@ export const projects = [
     description:
       "This website generates sports toys. Users can explore and add toys after registering or logging in. They can update, delete, and view all added toys.",
     src: "https://i.ibb.co/zmp7ZQC/Sports-Academy-Home.png",
-    technologies : "react, express.js, tailwind, css, MongoDB",
+    technologies: "react, express.js, tailwind, css, MongoDB",
     link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
     color: "rgb(219 39 119)",
+    github: "https://github.com/Shakil57375/toy-generator-client",
+    liveLink: "https://assignment-eleven-7252c.web.app/",
   },
   {
     title: "Sports Kidzy",
     workingMnt: "Nov-Dec 2023",
-    technologies : "react, express.js, tailwind, css, MongoDB, stripe",
+    technologies: "react, express.js, tailwind, css, MongoDB, stripe",
     description:
       "This website is for a summer sports academy where users can sign up for classes taught by instructors and pay using Stripe. Instructors can add classes, track enrollment and receive feedback, while admins manage users and classes, and provide feedback.",
     src: "https://i.ibb.co/pzJ5K5N/Sporty-Kidz-Home.png",
     link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
     color: "rgb(190 18 60)",
+    github: "https://github.com/Shakil57375/sports-academy-client",
+    liveLink: "https://sports-academy-special.web.app/",
   },
 ];

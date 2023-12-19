25a58021b77b8c4d5ae64577c23dd0379807aa81
Shakil57375 <shakil57375@gmail.com>
2023-12-19 14:46:23 +0600

    package installed

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -12,7 +12,7 @@ const Main = () => {
       <FloatingNav />
       {/* //Todo : uncomment banner */}
       {/* <Banner /> */}
-      <AboutMe />
+      {/* <AboutMe /> */}
       <Skills />
       {/* <Projects /> */}
       {/* <Contact /> */}

331c29f15f5422cc4461461eb83a57db6bc4064f
Shakil57375 <shakil57375@gmail.com>
2023-12-19 22:55:38 +0600

    projects links added

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,6 +5,7 @@ import Lenis from "@studio-freight/lenis";
 import ProjectCard from "./ProjectCard";
 import SectionTitle from "../Utils/SectionTitle";
 import GradientBackground from "./GradientBackground";
+import { ImCross } from "react-icons/im";
 const Projects = () => {
   const container = useRef(null);
   const { scrollYProgress } = useScroll({
@@ -27,13 +28,14 @@ const Projects = () => {
         <SectionTitle
           titleFirstWord={"My Projects"}
           titleFirstClassName={"text-8xl text-white font-bold font-Montserrat"}
-          divClassName={"relative top-10 flex justify-center "}
+          divClassName={"relative top-10 left-28 flex justify-center "}
         ></SectionTitle>
         <GradientBackground
           className={
             "absolute top-0 -right-48 bg-blue-600 blur-[120px] w-[400px] opacity-80 h-40"
           }
         />
+        <ImCross className="text-6xl absolute top-40 right-16 text-white" />
       </div>
       <main ref={container} className="relative mt-32">
         {projects.map((project, i) => {

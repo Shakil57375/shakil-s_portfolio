63fab291b9a4b06dec525ad543afc138b3006f5c
Shakil57375 <shakil57375@gmail.com>
2023-12-20 08:16:14 +0600

    new data added

--- src/Data/projectData.js
+++ src/Data/projectData.js
@@ -1,19 +1,19 @@
 export const projects = [
   {
-    title: "Recipe Hunter",
-    workingMnt: "Aug-Sep 2023",
+    title: "The Universal News",
+    workingMnt: "Sep-Oct 2023",
     description:
       "This is a recipe website featuring top-rated chefs and their recipes from Bangladesh. To access the best chefs' recipes, you'll need to log in. On a chef's details page, you'll find in-depth information about the chef and their recipes.",
-    src: "https://i.ibb.co/QF246Bh/Recipe-hunter.png",
+    src: "https://i.ibb.co/RSg1yQ6/The-Universal-News.png",
     technologies: "react, express.js, tailwind, css",
     link: "https://www.ignant.com/2023/03/25/ad2186-matthias-leidingers-photographic-exploration-of-awe-and-wonder/",
     color: "rgb(15 118 110)",
-    github: "https://github.com/Shakil57375/chef-hunter-client",
-    liveLink: "https://assignment-ten-1b552.web.app/",
+    github: "https://github.com/mthtitumir/the-universal-news",
+    liveLink: "https://the-universal-news.vercel.app/",
   },
   {
     title: "Toy Generator",
-    workingMnt: "Oct-Nov 2023",
+    workingMnt: "May-June 2023",
     description:
       "This website generates sports toys. Users can explore and add toys after registering or logging in. They can update, delete, and view all added toys.",
     src: "https://i.ibb.co/zmp7ZQC/Sports-Academy-Home.png",
@@ -25,7 +25,7 @@ export const projects = [
   },
   {
     title: "Sports Kidzy",
-    workingMnt: "Nov-Dec 2023",
+    workingMnt: "June-July 2023",
     technologies: "react, express.js, tailwind, css, MongoDB, stripe",
     description:
       "This website is for a summer sports academy where users can sign up for classes taught by instructors and pay using Stripe. Instructors can add classes, track enrollment and receive feedback, while admins manage users and classes, and provide feedback.",

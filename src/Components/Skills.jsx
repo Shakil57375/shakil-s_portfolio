b03d63c2b753bfc15f6adaf850f7c80e13b56794
Shakil57375 <shakil57375@gmail.com>
2023-12-22 09:37:01 +0600

    skills  mobile  responsive added

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -22,7 +22,7 @@ const Skills = () => {
       <GradientBackground
         className={`absolute top-80 left-4 bg-purple-700 blur-[120px] w-96 h-80 opacity-40`}
       />
-      <div className="flex flex-col lg:flex-row items-center justify-between gap-10">
+      <div className="flex flex-col lg:flex-row items-center justify-between lg:gap-10 gap-0">
         <div className="mt-32 flex flex-col gap-5">
           <div className="flex gap-16 lg:gap-[120px] items-center">
             <p className="text-2xl font-semibold text-white">HTML</p>
@@ -101,7 +101,7 @@ const Skills = () => {
           </div>
         </div>
         <div className="mt-32 flex flex-col gap-5">
-          <div className="flex gap-[102px] lg:gap-[136px] items-center">
+          <div className="flex gap-[80px] lg:gap-[136px] items-center">
             <p className="text-2xl font-semibold text-white">Node</p>
             <div className="flex gap-2 lg:gap-4">
               <div className="w-5 h-5 rounded-full bg-rose-500"></div>
@@ -116,7 +116,7 @@ const Skills = () => {
               <div className="w-5 h-5 rounded-full bg-gray-700"></div>
             </div>
           </div>
-          <div className="flex gap-[83px]  lg:gap-[115px] items-center">
+          <div className="flex gap-[59px]  lg:gap-[115px] items-center">
             <p className="text-2xl font-semibold text-white">Express</p>
             <div className="flex gap-2 lg:gap-4">
               <div className="w-5 h-5 rounded-full bg-rose-500"></div>
@@ -131,7 +131,7 @@ const Skills = () => {
               <div className="w-5 h-5 rounded-full bg-gray-700"></div>
             </div>
           </div>
-          <div className="flex gap-[52px] lg:gap-[84px] items-center">
+          <div className="flex gap-[28px] lg:gap-[84px] items-center">
             <p className="text-2xl font-semibold text-white">MongoDB</p>
             <div className="flex gap-2 lg:gap-4">
               <div className="w-5 h-5 rounded-full bg-rose-500"></div>
@@ -146,7 +146,7 @@ const Skills = () => {
               <div className="w-5 h-5 rounded-full bg-gray-700"></div>
             </div>
           </div>
-          <div className="flex gap-[88px] lg:gap-[120px] items-center">
+          <div className="flex gap-[64px] lg:gap-[120px] items-center">
             <p className="text-2xl font-semibold text-white">Next.js</p>
             <div className="flex gap-2 lg:gap-4">
               <div className="w-5 h-5 rounded-full bg-rose-500"></div>
@@ -161,7 +161,7 @@ const Skills = () => {
               <div className="w-5 h-5 rounded-full bg-gray-700"></div>
             </div>
           </div>
-          <div className="flex gap-[52px] lg:gap-[84px] items-center">
+          <div className="flex gap-[27px] lg:gap-[84px] items-center">
             <p className="text-2xl font-semibold text-white">Typescript</p>
             <div className="flex gap-2 lg:gap-4">
               <div className="w-5 h-5 rounded-full bg-rose-500"></div>

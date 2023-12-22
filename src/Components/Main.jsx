cb46818be7464acac82b85e615e0a04ff47d61cd
Shakil57375 <shakil57375@gmail.com>
2023-12-22 19:10:27 +0600

    package unistalled  added

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -6,48 +6,18 @@ import Contact from "./Contact";
 import BottomToTop from "./BottomToTop";
 import Skills from "./Skills";
 import Blogs from "./Blogs";
-import { useState } from "react";
-import "./Main.css"
 
 const Main = () => {
-  const [cursorPosition, setCursorPosition] = useState({ top: 0, left: 0 });
-  const [expand, setExpand] = useState(false);
-
-  const handleMouseMove = (e) => {
-    setCursorPosition({
-      top: e.pageY - 10,
-      left: e.pageX - 10,
-    });
-  };
-
-  const handleMouseClick = () => {
-    setExpand(true);
-
-    setTimeout(() => {
-      setExpand(false);
-    }, 500);
-  };
-
   return (
     <div className="">
-      <div
-        className={`cursor ${expand ? "expand" : ""}`}
-        style={{
-          top: `${cursorPosition.top}px`,
-          left: `${cursorPosition.left}px`,
-        }}
-      ></div>
-
-      <div onMouseMove={handleMouseMove} onClick={handleMouseClick}>
-        <FloatingNav />
-        <Banner />
-        <AboutMe />
-        <Skills />
-        <Projects />
-        <Blogs />
-        <Contact />
-        <BottomToTop />
-      </div>
+      <FloatingNav />
+      <Banner />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <Blogs />
+      <Contact />
+      <BottomToTop />
     </div>
   );
 };

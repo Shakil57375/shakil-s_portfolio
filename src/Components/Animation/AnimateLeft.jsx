a9333d8717e7e0dd484261b64bf10f8e3ec15103
Shakil57375 <shakil57375@gmail.com>
2023-12-23 18:47:19 +0600

    animaiton added on the banner

---
+++ src/Components/Animation/AnimateLeft.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { motion, useInView, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
+const AnimateLeft = ({ children }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+  }, [isInView]);
+  return (
+    <div ref={ref} className="relative overflow-hidden w-fit z-40">
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, x: -1075 },
+          visible: { opacity: 1, x: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.8, delay: 0.45 }}
+        className="z-40"
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
+};
+
+export default AnimateLeft;
